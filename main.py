ce2a1d183e098fac81b811495f9cd911c191559b
Bamba61 <brouhh05@gmail.com>
2025-08-26 03:30:22 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,58 +6,58 @@ response = requests.get(apiurl)
 data = response.json() 
 someword = random.choice(data)
 
-
-
 def loop():
-
-    
-    while True:
-        guess = input("Enter a word: " ) 
+    count = 0
+    while True: 
+        guess = input("Enter a word: ") 
+        count += 1
         let_check(guess)
         if len(guess) == len(someword):
             print(result)
-        if guess  == someword:
-            correct(guess)
+        correct(guess)
+        if count == 6:
+            print("The word was: " + someword)
             break
-        
-        
-def count_check():
-    count = 0
-    while count != 6:
-        loop()
-        count += 1
-    print("The word was: " + someword)
-
-            
+    
 def let_check(guess):
     global result  
     result = ""
     for x in someword:
         wordLet = x
-        for i in guess:
-            newword = i
-            if guess != someword and wordLet == newword:
-                result += newword 
-    if len(guess) < len(someword):
+    for i in guess:
+        newword = i
+        if wordLet == newword:
+                result += newword
+    if len(guess) > len(someword):
         print("Too Many Words Try Again")
-    elif len(guess) > len(someword):
+    elif len(guess) < len(someword):
         print("Too Little Words Try Again")
     
-            
-                
 def correct(guess):
-    
     if guess == someword:
         print("Ding, Ding, Ding!!")
             
+loop()
 
-    
-count_check()
+
+                  
+             
+            
+
+
+
+
+
+
+
+
+
 
 
                   
              
           
+
 
 
 
